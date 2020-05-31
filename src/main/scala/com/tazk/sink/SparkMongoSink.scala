8299a7c7cbb6239401344e796ee5b520cf5efdda
zhang_aiping <zhang_aiping@icekredit.com>
2020-05-31 12:12:42 +0800

    Fix: 添加删除mongo中多余数据的操作

--- src/main/scala/com/tazk/sink/SparkMongoSink.scala
+++ src/main/scala/com/tazk/sink/SparkMongoSink.scala
@@ -1,22 +1,22 @@
 package com.tazk.sink
 
+import java.lang.{Object => JObject}
+import java.util.{List => JList, Map => JMap}
+
 import com.mongodb.client.MongoCollection
-import com.mongodb.spark.{MongoConnector, MongoSpark}
+import com.mongodb.client.model.{DeleteOneModel, Filters, UpdateOneModel, UpdateOptions}
 import com.mongodb.spark.config.{ReadConfig, WriteConfig}
+import com.mongodb.spark.{MongoConnector, MongoSpark}
+import com.tazk.common.TazkCommon
 import com.tazk.deploy.TazkMongoUpdateModeAction
 import com.tazk.deploy.TazkMongoUpdateModeAction.TazkMongoUpdateModeAction
 import com.tazk.util.Utils
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.util.LongAccumulator
 import org.bson.Document
 
-import scala.collection.mutable
-import java.lang.{Object => JObject}
-import java.util.{Map => JMap, List => JList}
-
-import com.tazk.common.TazkCommon
-import org.apache.spark.util.LongAccumulator
-
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 /**
  *
@@ -46,6 +46,11 @@ class SparkMongoSink(spark: SparkSession,
   import spark.implicits._
 
   /**
+   * 是否进行转换
+   */
+  override val camelConvertBool: Boolean = camelConvert
+
+  /**
    * 写入目标库
    *
    * @return 返回写入成功条数
@@ -66,23 +71,54 @@ class SparkMongoSink(spark: SparkSession,
       case TazkMongoUpdateModeAction.ALLOW_UPDATE =>
         val updateResult = update(dataset, readConfig, writeConfig)
         (updateResult._1, updateResult._2, 0)
-      case TazkMongoUpdateModeAction.ALLOW_DELETE => ???
+      case TazkMongoUpdateModeAction.ALLOW_DELETE => delete(dataset, readConfig, writeConfig)
+
       case _ => throw new IllegalArgumentException(s"[$updateMode]不支持到类型")
     }
   }
 
   /**
+   * 需要删除到数据
+   *
+   * @param dataset     Dataset[Row
+   * @param readConfig  mongo读取配置信息
+   * @param writeConfig mongo写入配置信息
+   * @return
+   */
+  private def delete(dataset: Dataset[Row], readConfig: ReadConfig, writeConfig: WriteConfig): (Long, Long, Long) = {
+    val updateInfo = update(dataset, readConfig, writeConfig)
+    val deleteCount = spark.sparkContext.longAccumulator("DELETE_MONGO_COUNT")
+
+    val deleteAlias = "his_del_ds"
+    val mongoDS = readMongoHistory(readConfig, deleteAlias)
+    val hisCols = mongoDS.columns
+    val deleteData = dataset.alias("cur_ds").join(mongoDS, $"$updateKey", "right join")
+      .where(s"cur_ds.$updateKey is null")
+      .selectExpr(hisCols.map(c => s"$deleteAlias.$c"): _*)
+
+    // 批量删除
+    operateMongo[DeleteOneModel[Document]](deleteData, writeConfig, deleteCount, (collection, docList) => {
+      val delete = docList.asScala.map { doc =>
+        new DeleteOneModel[Document](Filters.eq(s"$updateKey", doc.get(updateKey)))
+      }
+      collection.bulkWrite(delete.asJava)
+    })
+
+    (updateInfo._1, updateInfo._2, deleteCount.value)
+  }
+
+  /**
    * 数据更新到mongo
    *
-   * @param dataset    Dataset[Row]
-   * @param readConfig mongo配置信息
+   * @param dataset     Dataset[Row]
+   * @param readConfig  mongo配置信息
+   * @param writeConfig mongo写入配置信息
    * @return 成功更新条数和新增条数
    */
   private def update(dataset: Dataset[Row], readConfig: ReadConfig, writeConfig: WriteConfig): (Long, Long) = {
     // mongo中现存的数据
-    val mongoDS = spark.read.json(MongoSpark.load(spark.sparkContext, readConfig)
-      .mapPartitions(_.map(content2JSON)).toDS)
-      .alias("his_ds")
+    val updateAlias = "his_up_ds"
+    val mongoDS = readMongoHistory(readConfig, updateAlias)
     val aliasCurDS = dataset.alias("cur_ds")
 
     val curCols = aliasCurDS.columns
@@ -90,15 +126,24 @@ class SparkMongoSink(spark: SparkSession,
 
     // 需要新增添加的数据
     val preInsert = aliasCurDS.join(mongoDS, $"$updateKey", "left semi join")
-      .where(s"his_ds.$updateKey is null")
+      .where(s"$updateAlias.$updateKey is null")
+    val updateMongoCount = spark.sparkContext.longAccumulator("UPDATE_MONGO_COUNT")
     val insertCount = insert(preInsert, writeConfig)
 
     // 需要更新的数据
-    val updateMongoCount = spark.sparkContext.longAccumulator("UPDATE_MONGO_COUNT")
     val updateData = aliasCurDS.join(mongoDS, $"$updateKey", "inner join")
-      .selectExpr(Utils.findColNams(curCols, hisCols, "cur_ds", "his_ds",
-        ignoreUpdateKey.getOrElse("") ++ "his_ds._id"): _*)
-    intoMongo(updateData, writeConfig, updateMongoCount, (collection, docList) => collection.bulkWrite(docList))
+      .selectExpr(Utils.findColNams(curCols, hisCols, "cur_ds", updateAlias,
+        ignoreUpdateKey.getOrElse("") ++ s"$updateAlias._id"): _*)
+    // 批量更新
+    operateMongo[UpdateOneModel[Document]](updateData, writeConfig, updateMongoCount, (collection, docList) => {
+      val upsert = docList.asScala.map { doc =>
+        val modifiers = new Document()
+        modifiers.put("$set", doc)
+        new UpdateOneModel[Document](Filters.eq(s"$updateKey", doc.get(updateKey)),
+          modifiers, new UpdateOptions().upsert(true))
+      }
+      collection.bulkWrite(upsert.asJava)
+    })
 
     (insertCount, updateMongoCount.value)
   }
@@ -112,7 +157,7 @@ class SparkMongoSink(spark: SparkSession,
    */
   private def insert(dataset: Dataset[Row], writeConfig: WriteConfig): Long = {
     val insertMongoCount = spark.sparkContext.longAccumulator("INSERT_MONGO_COUNT")
-    intoMongo(dataset, writeConfig, insertMongoCount, (collection, docList) => collection.insertMany(docList))
+    operateMongo[Document](dataset, writeConfig, insertMongoCount, (collection, docList) => collection.insertMany(docList))
     insertMongoCount.value
   }
 
@@ -124,8 +169,8 @@ class SparkMongoSink(spark: SparkSession,
    * @param longAccumulator 计数器
    * @param intoMongoFun    进入mongo的方式，insert或者update
    */
-  private def intoMongo(dataset: Dataset[Row], writeConfig: WriteConfig,
-                        longAccumulator: LongAccumulator, intoMongoFun: (MongoCollection[Document], JList[Document]) => Unit): Unit = {
+  private def operateMongo[D](dataset: Dataset[Row], writeConfig: WriteConfig,
+                              longAccumulator: LongAccumulator, intoMongoFun: (MongoCollection[D], JList[Document]) => Unit): Unit = {
     dataset.foreachPartition(rowPartition => {
       // 创建Mongoecotr
       val mongoConnector = MongoConnector(writeConfig.asOptions)
@@ -136,7 +181,7 @@ class SparkMongoSink(spark: SparkSession,
 
       if (rowPartition.nonEmpty) {
         // 将Row转换成Document后，再进行批量插入
-        mongoConnector.withCollectionDo(writeConfig, { collection: MongoCollection[Document] =>
+        mongoConnector.withCollectionDo(writeConfig, { collection: MongoCollection[D] =>
           val docList = rowPartition.map(row => {
             val valMap: JMap[String, JObject] = (for (index <- 0 until row.size) yield {
               colNames(index) -> row.get(index).asInstanceOf[JObject]
@@ -153,7 +198,13 @@ class SparkMongoSink(spark: SparkSession,
   }
 
   /**
-   * 是否进行转换
+   * 读取mongo历史数据
+   *
+   * @param readConfig mongo配置
    */
-  override val camelConvertBool: Boolean = camelConvert
+  private def readMongoHistory(readConfig: ReadConfig, aliasName: String): Dataset[Row] = {
+    spark.read.json(MongoSpark.load(spark.sparkContext, readConfig)
+      .mapPartitions(_.map(content2JSON)).toDS)
+      .alias(aliasName)
+  }
 }
